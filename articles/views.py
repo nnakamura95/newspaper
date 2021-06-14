923f46b89db32c7cb2df37837b006f6a6dfa1f48
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2021-06-14 23:26:49 +0900

    Added UserPassesTestMixin

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin, UserPassesTestMixin)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (UpdateView, DeleteView, CreateView)
 from django.urls import reverse_lazy
@@ -15,16 +15,24 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
