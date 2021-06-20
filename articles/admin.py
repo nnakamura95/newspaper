37912739711321f9e0cc90fe5cc406a0f3bb88bf
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2021-06-20 23:31:35 +0900

    Modified articles/admin to implement TabularInline

--- articles/admin.py
+++ articles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Article, Comment
 
 
-class CommentInline(admin.StackedInline):
+class CommentInline(admin.TabularInline):
     model = Comment
 
 
